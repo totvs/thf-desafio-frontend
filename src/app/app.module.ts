3d9fe45c50486068885c70e30b5a219b2c2035fd
Adler Coelho Santos <adler.coelho.santos@icloud.com>
2018-07-06 18:17:33 -0300

    Modifying angular-cli file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { RangeComponent } from './components/range/range.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RangeComponent
   ],
   imports: [
     BrowserModule
