63e6a5274fc86a1bab676c5f947ad3b0554f2eb3
Aline Lariguet <alinecbgmoreira@gmail.com>
2018-08-16 15:23:51 -0300

    testes thf-range

--- src/app/thf-range/thf-range.component.spec.ts
+++ src/app/thf-range/thf-range.component.spec.ts
@@ -22,4 +22,37 @@ describe('ThfRangeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('o inicio não pode ser menor que o minimo - caso seja, inicio = minimo.', () => {
+    component.minimo = 10;
+    component.inicio = 5;
+    component.ngOnInit();
+    expect(component.inicio).toEqual(component.minimo);
+  });
+
+  it('o fim não pode ser maior que o maximo - caso seja, fim = maximo.', () => {
+    component.maximo = 5;
+    component.fim = 10;
+    component.ngOnInit();
+    expect(component.fim).toEqual(component.maximo);
+  });
+
+  it('caso o inicio não seja informado, inicio = minimo.', () => {
+    component.minimo = 0;
+    component.ngOnInit();
+    expect(component.inicio).toEqual(component.minimo);
+  });
+
+  it('caso o fim não seja informado, fim = maximo.', () => {
+    component.maximo = 100;
+    component.ngOnInit();
+    expect(component.fim).toEqual(component.maximo);
+  });
+
+  it('caso o minimo e o maximo não forem informados, minimo = 0 e maximo = 100', () => {
+    component.ngOnInit();
+    expect(component.minimo).toEqual(0);
+    expect(component.maximo).toEqual(100);
+  });
+
 });
