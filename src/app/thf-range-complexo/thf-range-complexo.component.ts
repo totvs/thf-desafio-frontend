07190159507e140cc01eaee7199b466cf57de1ba
Aline Lariguet <alinecbgmoreira@gmail.com>
2018-08-16 16:43:47 -0300

    inclusão dos atributos do thf-input-range-complexo e alterações no html

--- src/app/thf-range-complexo/thf-range-complexo.component.ts
+++ src/app/thf-range-complexo/thf-range-complexo.component.ts
@@ -7,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThfRangeComplexoComponent implements OnInit {
 
+  casas: any[];
+
+  casasFiltradas: any[] = [];
+
+  menorValor: number;
+  
+  maiorValor:number;
+
   constructor() { }
+
+  recebeMenorMaior(resposta) {
+    this.atualizaValores(resposta.menorAtual, resposta.maiorAtual);
+  }
+
+  atualizaValores(menorAtual:number, maiorAtual:number){
+    this.casasFiltradas = [];
+    for(let i = 0; i< this.casas.length; i++){
+      if(this.casas[i].valor >= menorAtual && this.casas[i].valor <= maiorAtual){
+        this.casasFiltradas.push(this.casas[i]);
+      }
+    }
+  
+  }
 
   ngOnInit() {
   }
