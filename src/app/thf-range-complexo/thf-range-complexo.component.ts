fc136ff39cd907024f5b1c881c949b8ea171494a
Aline Lariguet <alinecbgmoreira@gmail.com>
2018-08-16 16:31:47 -0300

    inclusão do thf-range-complexo no app

--- src/app/thf-range-complexo/thf-range-complexo.component.ts
+++ src/app/thf-range-complexo/thf-range-complexo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-thf-range-complexo',
+  selector: 'thf-range-complexo',
   templateUrl: './thf-range-complexo.component.html',
   styleUrls: ['./thf-range-complexo.component.css']
 })
