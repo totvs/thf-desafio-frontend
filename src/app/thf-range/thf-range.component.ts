4f4ca74b222c4f5490dc6310ee6ba6f828591226
Aline Lariguet <alinecbgmoreira@gmail.com>
2018-08-16 12:23:50 -0300

    criação dos atributos necessários thf-range

--- src/app/thf-range/thf-range.component.ts
+++ src/app/thf-range/thf-range.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'thf-range',
@@ -6,6 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./thf-range.component.css']
 })
 export class ThfRangeComponent implements OnInit {
+
+  @Input("t-min")
+  public minimo: number;
+
+  @Input("t-max")
+  public maximo: number;
+
+  @Input("t-inicio")
+  public inicio: number;
+
+  @Input("t-fim")
+  public fim: number;
+
+  @Input("t-step")
+  public step: number;
+
+  @Input("t-com-indicadores")
+  public comIndicadores: boolean;
+
+  @Input("t-disabled")
+  public disabled: boolean;
+  
+  @Input("t-moeda")
+  public moeda: boolean;
+
+  @Input("t-label")
+  public label: string;
+
+  @Output("t-change") 
+  public menorMaiorValorRangeEmitter = new EventEmitter();
+
+  @ViewChild("htmlInputRangeOriginal") 
+  private htmlInputRangeOriginal: ElementRef;
+  
+  @ViewChild("htmlInputRangeAuxiliar") 
+  private htmlInputRangeAuxiliar: ElementRef;
+
+  private menorValorRange: number;
+
+  private maiorValorRange: number;
 
   constructor() { }
 
