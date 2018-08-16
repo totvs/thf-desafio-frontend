3f568897cdc8f3de03d27349672adadcb53b4742
Aline Lariguet <alinecbgmoreira@gmail.com>
2018-08-16 00:37:41 -0300

    inclusão do thf-range no app-component

--- src/app/thf-range/thf-range.component.ts
+++ src/app/thf-range/thf-range.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-thf-range',
+  selector: 'thf-range',
   templateUrl: './thf-range.component.html',
   styleUrls: ['./thf-range.component.css']
 })
